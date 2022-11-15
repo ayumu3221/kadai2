7649e195111dc6d3dfaf24a45e39715577ebe35f
EC2 Default User <117354218+ayumu3221@users.noreply.github.com>
2022-11-15 08:35:16 +0000

    ”Finish”

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_13_161527) do
+ActiveRecord::Schema.define(version: 2022_11_13_182413) do
 
-  create_table "lists", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
